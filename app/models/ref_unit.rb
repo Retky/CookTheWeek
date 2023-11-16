73c9c0a8b77070523367415590e81de896280b52
Retky <retkyfox@gmail.com>
2023-11-16 12:09:12 -0600

    Add field sanitization and validations to RefUnit model

--- app/models/ref_unit.rb
+++ app/models/ref_unit.rb
@@ -1,2 +1,23 @@
 class RefUnit < ApplicationRecord
+  before_validation :sanitize_fields
+
+  # Validations
+  validates :name, presence: true
+  validates :abbreviation, presence: true
+  validates :type, presence: true
+  validates :unit_factor, presence: true
+  validates :unit_reference, presence: true
+
+  def sanitize_fields
+    self.name = sanitize_strings(self.name)
+    self.type = sanitize_strings(self.type)
+    self.unit_reference = sanitize_strings(self.unit_reference)
+  end
+
+  def sanitize_strings(string)
+    return string if string.blank?
+
+    sanitized_string = string.downcase.capitalize
+    sanitized_string.gsub(/\b([a-z])/) { $1.capitalize }
+  end
 end
