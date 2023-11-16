f462fac7774efa34c689904c5b84fd93c22286d2
Retky <retkyfox@gmail.com>
2023-11-16 14:15:01 -0600

    Mod string sanitization in RefUnit model.

--- app/models/ref_unit.rb
+++ app/models/ref_unit.rb
@@ -1,7 +1,6 @@
 class RefUnit < ApplicationRecord
   before_validation :sanitize_fields
 
-  # Validations
   validates :name, presence: true
   validates :abbreviation, presence: true
   validates :category, presence: true
@@ -14,10 +13,11 @@ class RefUnit < ApplicationRecord
     self.unit_reference = sanitize_strings(self.unit_reference)
   end
 
+  # Capitalize the first letter of each word in a string.
   def sanitize_strings(string)
     return string if string.blank?
 
-    sanitized_string = string.downcase.capitalize
+    sanitized_string = string.downcase
     sanitized_string.gsub(/\b([a-z])/) { $1.capitalize }
   end
 end
