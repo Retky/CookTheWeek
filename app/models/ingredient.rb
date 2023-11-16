f65059d15203581aae29708a15f25514e2eed8a0
Retky <retkyfox@gmail.com>
2023-11-16 14:46:02 -0600

    Add name sanitization and validation to Ingredient model.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,18 @@
 class Ingredient < ApplicationRecord
-  belongs_to :user
+  before_validation :sanitize_name
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  has_many :user_ingredients, dependent: :destroy
+  has_many :users, through: :user_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
+  has_many :shop_list_ingredients, dependent: :destroy
+  has_many :shop_lists, through: :shop_list_ingredients
+
+  # Capitalize the first letter & remove any punctuation
+  def sanitize_name
+    return if name.blank?
+    self.name = name.downcase.gsub(/[^a-zA-Z0-9 ]/, '').strip.capitalize
+  end
 end
