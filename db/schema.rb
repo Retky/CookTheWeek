24be8d5c7c3475d15e8198528084102c91fdfab2
Retky <retkyfox@gmail.com>
2023-11-16 14:22:00 -0600

    Add user_ingredients table to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_171209) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_201723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ingredients", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_ingredients_on_user_id"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -120,6 +118,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_171209) do
     t.index ["user_id"], name: "index_shop_lists_on_user_id"
   end
 
+  create_table "user_ingredients", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_user_ingredients_on_ingredient_id"
+    t.index ["user_id"], name: "index_user_ingredients_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -133,7 +140,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_171209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "ingredients", "users"
   add_foreign_key "meals", "users"
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
@@ -145,4 +151,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_171209) do
   add_foreign_key "shop_list_meals", "meals"
   add_foreign_key "shop_list_meals", "shop_lists"
   add_foreign_key "shop_lists", "users"
+  add_foreign_key "user_ingredients", "ingredients"
+  add_foreign_key "user_ingredients", "users"
 end
