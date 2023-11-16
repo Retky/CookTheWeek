2a36dc54be8fc07774f55070f78b8e20636317b5
Retky <retkyfox@gmail.com>
2023-11-16 16:05:32 -0600

    Add recipe associations and validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :recipe_steps, dependent: :destroy
+
+  validates :name, presence: true
+  validates :portions, presence: true, default: 1, numericality: { greater_than: 0 }
+  validates :preparation_time, presence: true, default: 0, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, default: 0, numericality: { greater_than_or_equal_to: 0 }
+  validates :public, inclusion: { in: [true, false] }, default: false
 end
