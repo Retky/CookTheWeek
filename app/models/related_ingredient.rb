3c350a4b051d761475fcc1220f949f6ae6849d96
Retky <retkyfox@gmail.com>
2023-11-17 14:19:56 -0600

    Add unit conversion method to RelatedIngredient model

--- app/models/related_ingredient.rb
+++ app/models/related_ingredient.rb
@@ -1,3 +1,20 @@
 class RelatedIngredient < ApplicationRecord
   belongs_to :ingredient
+
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :unit, presence: true
+
+  def convert_unit(new_unit)
+    return if unit == new_unit
+
+    if quantity.zero?
+      self.unit = new_unit
+    else
+      current_unit = RefUnit.find_by(name: unit)
+      next_unit = RefUnit.find_by(name: new_unit)
+
+      self.unit = new_unit
+      self.quantity = (quantity * current_unit.unit_factor) / next_unit.unit_factor
+    end
+  end
 end
