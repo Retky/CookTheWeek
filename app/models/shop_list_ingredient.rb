a253e868c5c5c17ab04b7e01d6137ca1226a9d71
Retky <retkyfox@gmail.com>
2023-11-17 15:43:32 -0600

    Fix quantity calculation in sum_quantity method

--- app/models/shop_list_ingredient.rb
+++ app/models/shop_list_ingredient.rb
@@ -2,7 +2,7 @@ class ShopListIngredient < RelatedIngredient
   belongs_to :shop_list
 
   def sum_quantity(new_value)
-    quantity += new_value
+    self.quantity += new_value
     save
   end
 end
