9e1a93e3b98127030c88b1c4be4b91ac35ec33b5
Retky <retkyfox@gmail.com>
2023-11-20 22:34:08 -0600

    Add ingredients controller and route

---
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::IngredientsController < ApplicationController
+  # All ingredients
+  def index
+    @ingredients = Ingredient.all
+    render json: @ingredients
+  end
+end
---
+++ spec/requests/api/v1/ingredients_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Ingredients", type: :request do
+  before do
+    Ingredient.create(name: "Milk")
+    Ingredient.create(name: "Eggs")
+    Ingredient.create(name: "Flour")
+    
+    get "/api/v1/ingredients"
+  end
+
+  describe "GET /index" do
+    it "returns success status" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns a proper JSON" do
+      expect(response.content_type).to include("application/json")
+    end
+
+    it "returns all the ingredients" do
+      parsed_body = JSON.parse(response.body)
+
+      expect(parsed_body.length).to eq(3)
+      expect(parsed_body[0]["name"]).to eq("Milk")
+      expect(parsed_body[1]["name"]).to eq("Eggs")
+      expect(parsed_body[2]["name"]).to eq("Flour")
+    end
+  end
+end
