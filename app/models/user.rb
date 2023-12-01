2e9426789db0b42a73a25a0bf4f1ed759531bc01
Retky <retkyfox@gmail.com>
2023-12-01 13:36:18 -0600

    Mod jwt_revocation_strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable,
-         jwt_revocation_strategy: self
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   has_many :recipes, dependent: :destroy
   has_many :shop_lists, dependent: :destroy
