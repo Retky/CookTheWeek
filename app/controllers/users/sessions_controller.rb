5d154a9db73793d5342d2eb2b2900efb180e94bc
Retky <retkyfox@gmail.com>
2023-12-05 17:16:18 -0600

    Revert "Mod registration and session controllers"
    This reverts commit c1772c2203a9f8e5c4c0af532b02cf3a8b71c6eb.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,58 @@
-# app/controllers/users/sessions_controller.rb
 class Users::SessionsController < Devise::SessionsController
+  include RackSessionsFix
   respond_to :json
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 
   private
 
-  def respond_with(_resource, _opts = {})
+  def respond_with(current_user, _opts = {})
     render json: {
-      message: 'You are logged in.',
-      user: current_user
+      status: {
+        code: 200, message: 'Logged in successfully.',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
     }, status: :ok
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
 
-    log_out_failure
-  end
-
-  def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
-  end
-
-  def log_out_failure
-    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 end
