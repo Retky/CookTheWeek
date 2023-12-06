56cb5357e8bfef2d4ade47710c27eb8da2e62a63
Retky <retkyfox@gmail.com>
2023-12-05 18:01:39 -0600

    Mod routes and migrations for devise-api

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-   controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  get 'pages/home'
+  get 'pages/restricted'
+  devise_for :users
 
   namespace :api do
     namespace :v1 do
