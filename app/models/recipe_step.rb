2080250f9aac7078a4c8ef2f28a5b39acac1c6fc
Retky <retkyfox@gmail.com>
2023-12-07 20:06:07 -0600

    Add validations for step_number and instructions in RecipeStep model

--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -1,3 +1,6 @@
 class RecipeStep < ApplicationRecord
   belongs_to :recipe
+
+  validates :step_number, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :instructions, presence: true
 end
