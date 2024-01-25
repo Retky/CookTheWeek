e681a07c9cd43ff31fdd91e942f2b699565f0e5b
Retky <retkyfox@gmail.com>
2024-01-25 13:53:19 -0600

    Add method to calculate all ingredients in ShopList

--- app/models/shop_list.rb
+++ app/models/shop_list.rb
@@ -4,4 +4,21 @@ class ShopList < ApplicationRecord
   has_many :ingredients, through: :shop_list_ingredients
   has_many :shop_list_meals, dependent: :destroy
   has_many :meals, through: :shop_list_meals
+
+  def all_ingredients
+    recipe_ingredients = self.meals.map(&:recipe_id).map { |id| RecipeIngredient.where(recipe_id: id) }.flatten
+  
+    grouped_ingredients = recipe_ingredients.group_by { |ingredient| [ingredient.ingredient_id, ingredient.unit] }
+
+    summed_ingredients = grouped_ingredients.map do |(ingredient_id, unit), ingredients|
+      {
+        name: ingredients.first.ingredient.name,
+        ingredient_id: ingredient_id,
+        unit: unit,
+        quantity: ingredients.sum(&:quantity)
+      }
+    end
+  
+    return summed_ingredients
+  end  
 end
